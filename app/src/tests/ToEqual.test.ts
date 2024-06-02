50b2940c2b29d4cd9843655631b60db3fb2f0f49
roll1226 <tomo_yasu_4724@icloud.com>
2024-06-03 07:36:53 +0900

    change: type

--- app/src/tests/ToEqual.test.ts
+++ app/src/tests/ToEqual.test.ts
@@ -1,15 +1,14 @@
 import { describe, expect, it } from "vitest";
 
-type CreateObjectType<T> = (a: T, b: T) => { a: T; b: T };
+type CreateObjectType<T, K> = (a: T, b: K) => { a: T; b: K };
 type CreateArrayType<T> = (...elements: [...T[]]) => T[];
-
-// sum.test.js
 
 describe("関数の戻り値のテスト", () => {
   it("オブジェクトが等しいことを確認", () => {
-    const createObject: CreateObjectType<number> = (a, b) => {
+    const createObject: CreateObjectType<number, number> = (a, b) => {
       return { a, b };
     };
+
     expect(createObject(1, 2)).toEqual({ a: 1, b: 2 });
   });
 
@@ -17,13 +16,18 @@ describe("関数の戻り値のテスト", () => {
     const createArray: CreateArrayType<number> = (...elements) => {
       return elements;
     };
+
     expect(createArray(1, 2, 3)).toEqual([1, 2, 3]);
   });
 
   it("ネストされたオブジェクトが等しいことを確認", () => {
-    const createObject: CreateObjectType<unknown> = (a, b) => {
+    const createObject: CreateObjectType<{ nested: number }, number> = (
+      a,
+      b
+    ) => {
       return { a, b };
     };
+
     expect(createObject({ nested: 1 }, 2)).toEqual({ a: { nested: 1 }, b: 2 });
   });
 
