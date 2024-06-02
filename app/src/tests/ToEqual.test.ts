0a6623f5b7e0d8afc71cc759885a912f1abfd93f
roll1226 <tomo_yasu_4724@icloud.com>
2024-06-03 07:32:51 +0900

    change: type

--- app/src/tests/ToEqual.test.ts
+++ app/src/tests/ToEqual.test.ts
@@ -3,30 +3,37 @@ import { describe, expect, it } from "vitest";
 type CreateObjectType<T> = (a: T, b: T) => { a: T; b: T };
 type CreateArrayType<T> = (...elements: [...T[]]) => T[];
 
-const createObject: CreateObjectType<unknown> = (a, b) => {
-  return { a, b };
-};
-
-const createArray: CreateArrayType<unknown> = (...elements) => {
-  return elements;
-};
-
 // sum.test.js
 
 describe("関数の戻り値のテスト", () => {
   it("オブジェクトが等しいことを確認", () => {
+    const createObject: CreateObjectType<number> = (a, b) => {
+      return { a, b };
+    };
     expect(createObject(1, 2)).toEqual({ a: 1, b: 2 });
   });
 
   it("配列が等しいことを確認", () => {
+    const createArray: CreateArrayType<number> = (...elements) => {
+      return elements;
+    };
     expect(createArray(1, 2, 3)).toEqual([1, 2, 3]);
   });
 
   it("ネストされたオブジェクトが等しいことを確認", () => {
+    const createObject: CreateObjectType<unknown> = (a, b) => {
+      return { a, b };
+    };
     expect(createObject({ nested: 1 }, 2)).toEqual({ a: { nested: 1 }, b: 2 });
   });
 
   it("ネストされた配列が等しいことを確認", () => {
+    const createArray: CreateArrayType<[number, number, ...number[]]> = (
+      ...elements
+    ) => {
+      return elements;
+    };
+
     expect(createArray([1, 2], [3, 4])).toEqual([
       [1, 2],
       [3, 4],
